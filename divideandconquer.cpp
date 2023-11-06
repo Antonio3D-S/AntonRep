7da97b988deaaa9bed388ce7b59c0233df4a58bb
Antonio3D-S <57459755+Antonio3D-S@users.noreply.github.com>
2023-11-06 19:54:10 -0400

    Update divideandconquer.cpp

--- divideandconquer.cpp
+++ divideandconquer.cpp
@@ -38,14 +38,14 @@ bool EsNumeroValido(string str) {
     }
 }
 
-int* obtenerprimerasubdivision(double num) {
+int* DivisionDeTerminos(double num) {
     int* arreglo = new int[7]; // Asigna dinámicamente un arreglo de 7 enteros
 
     // Obtener la parte entera del número
     int parte_entera = static_cast<int>(num);
 
     // Obtener la parte decimal del número
-    int parte_decimal_entera = static_cast<int>((num - parte_entera) * 100); // Multiplicar por 100 para obtener los centavos como número entero
+    int parte_decimal_entera = ((num - parte_entera) * 100); // Multiplicar por 100 para obtener los centavos como número entero
 
     // Calcular la subdivisión
     arreglo[0] = (parte_entera / 100000000); // Centenas de millones
@@ -77,7 +77,7 @@ string DecenasUnidadesEnTexto(int numero,bool un_uno, bool cent) {
 
     string texto;
 
-    if(un_uno == 1)
+    if(un_uno == true)
     {
         unidades[1] = "Un";
     }
@@ -115,24 +115,25 @@ void finality(int* arr)
 
     if (arr[0] + arr[1] != 0)
     {
-        texto += CentenasEnTexto(arr[0], arr[1] > 0) + " " + DecenasUnidadesEnTexto(arr[1], arr[0] == 0, false) + ((arr[0] + arr[1] > 1) ? " millones " : " millón ");
+        texto += CentenasEnTexto(arr[0], arr[1] > 0) + " " + DecenasUnidadesEnTexto(arr[1], arr[0] == 0, false) + ((arr[0] == 0 && arr[1] == 1) ? " Millon " : " Millones ");
     }
     if (arr[2] + arr[3] != 0)
     {
         texto += CentenasEnTexto(arr[2], arr[3] > 0) + " ";
         
-        texto += ((arr[2] == 0 && arr[3] == 1) ? "" : DecenasUnidadesEnTexto(arr[3], arr[2] == 0, false)) + " Mil ";
+        texto += ((arr[2] == 0 && arr[3] == 1) ? "" : DecenasUnidadesEnTexto(arr[3], arr[2] > 0, false)) + " Mil ";
     }
     if (arr[4] + arr[5] != 0)
     {
-        texto += CentenasEnTexto(arr[4], arr[5] > 0) + " " + DecenasUnidadesEnTexto(arr[5], arr[4] == 0, false);
+        texto += CentenasEnTexto(arr[4], arr[5] > 0) + " " + DecenasUnidadesEnTexto(arr[5], (arr[4] == 0), false);
     }
     
-    texto += " Pesos Dominicanos Con " + DecenasUnidadesEnTexto(arr[6], true, true) + " Centimos ";
+    texto += " Pesos Dominicanos Con " + DecenasUnidadesEnTexto(arr[6], arr[6] == 1, true) + " Centimos ";
 
     cout << texto;
 }
-int main() {
+int main() 
+{
     while (true) {
         double numero;
         string entrada;
@@ -145,7 +146,7 @@ int main() {
 
         if (EsNumeroValido(entrada)) {
             numero = stod(entrada);
-            int* resultado = obtenerprimerasubdivision(numero);
+            int* resultado = DivisionDeTerminos(numero);
 
             cout << endl;
             finality(resultado);
